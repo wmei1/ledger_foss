7931a0be62a00d4e8330aaeda87bb78dd2684624
wmei1 <wmei1@hawk.iit.edu>
2017-05-25 17:14:31 -0500

    Test environment setup

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question
 
@@ -17,11 +18,15 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         """ return the last five published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        #return Question.objects.filter(pub_date__lte=timezone.now()
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
