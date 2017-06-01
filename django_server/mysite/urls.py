af5fe1970421f414eaa564929c9fb701f3bf2b80
wmei1 <wmei1@hawk.iit.edu>
2017-05-31 21:06:39 -0500

    client-side javscript start and python gui

--- django_server/mysite/urls.py
+++ django_server/mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^ledger/', include('ledger.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
